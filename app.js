64328133e0e9fb2d276322547ad882710a931d5c
IrwanKhaliq <Irwan.khaliq10@gmail.com>
2020-04-24 00:04:02 +0700

    Change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const session = require('express-session')// sessions
 const routes = require('./routes')
 
 const app = express()
-const port = 3000
+const port = process.env.port ||  3000
 
 app.set('view engine', 'ejs')
 
